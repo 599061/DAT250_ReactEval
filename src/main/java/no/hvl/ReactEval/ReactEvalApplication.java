86327dc7760cfc3e3dcac5a9ca2bc9de02b0dcf7
599061 <dag__aa@hotmail.com>
2025-11-13 14:37:36 +0100

    first commit

---
+++ src/main/java/no/hvl/ReactEval/ReactEvalApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.ReactEval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactEvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/hvl/ReactEval/ReactEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.ReactEval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
